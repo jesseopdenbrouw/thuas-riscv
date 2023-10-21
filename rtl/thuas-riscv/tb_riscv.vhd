a7c3e8fe96ee8916d2f44352116a7e7ebc89dd1a
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2023-10-21 10:44:32 +0200

    Removed component description

--- rtl/thuas-riscv/tb_riscv.vhd
+++ rtl/thuas-riscv/tb_riscv.vhd
@@ -47,78 +47,7 @@ end entity tb_riscv;
 
 architecture sim of tb_riscv is
 
---component riscv is
---    generic (
---          -- The frequency of the system
---          SYSTEM_FREQUENCY : integer := 50000000;
---          -- Frequecy of the hardware clock
---          CLOCK_FREQUENCY : integer := 1000000;
---          -- RISCV E (embedded) of RISCV I (full)
---          HAVE_RISCV_E : boolean := false;
---          -- Do we have the integer multiply/divide unit?
---          HAVE_MULDIV : boolean := TRUE;
---          -- Fast divide (needs more area)?
---          FAST_DIVIDE : boolean := TRUE;
---          -- Do we have Zba (sh?add)
---          HAVE_ZBA : boolean := TRUE;
---          -- Do we enable vectored mode for mtvec?
---          VECTORED_MTVEC : boolean := TRUE;
---          -- Do we have registers is RAM?
---          HAVE_REGISTERS_IN_RAM : boolean := TRUE;
---          -- Do we have a bootloader ROM?
---          HAVE_BOOTLOADER_ROM : boolean := TRUE;
---          -- Address width in bits, size is 2**bits
---          ROM_ADDRESS_BITS : integer := 16;
---          -- Address width in bits, size is 2**bits
---          RAM_ADDRESS_BITS : integer := 15;
---          -- Do we have fast store?
---          HAVE_FAST_STORE => boolean := false;
---          -- Do we have UART1?
---          HAVE_UART1 : boolean := TRUE;
---          -- Do we have SPI1?
---          HAVE_SPI1 : boolean := TRUE;
---          -- Do we have SPI2?
---          HAVE_SPI2 : boolean := TRUE;
---          -- Do we have I2C1?
---          HAVE_I2C1 : boolean := TRUE;
---          -- Do we have I2C2?
---          HAVE_I2C2 : boolean := TRUE;
---          -- Do we have TIMER1?
---          HAVE_TIMER1 : boolean := TRUE;
---          -- Do we have TIMER2?
---          HAVE_TIMER2 : boolean := TRUE
---         );
---    port (I_clk : in std_logic;
---          I_areset : in std_logic;
---          -- GPIOA
---          I_gpioapin : in data_type;
---          O_gpioapout : out data_type;
---          -- UART1
---          I_uart1rxd : in std_logic;
---          O_uart1txd : out std_logic;
---          -- I2C1
---          IO_i2c1scl : inout std_logic;
---          IO_i2c1sda : inout std_logic;
---          -- I2C2
---          IO_i2c2scl : inout std_logic;
---          IO_i2c2sda : inout std_logic;
---          -- SPI1
---          O_spi1sck : out std_logic;
---          O_spi1mosi : out std_logic;
---          I_spi1miso : in std_logic;
---          O_spi1nss : out std_logic;
---          -- SPI2
---          O_spi2sck : out std_logic;
---          O_spi2mosi : out std_logic;
---          I_spi2miso : in std_logic;
---          -- TIMER2
---          O_timer2oct : out std_logic;
---          IO_timer2icoca : inout std_logic;
---          IO_timer2icocb : inout std_logic;
---          IO_timer2icocc : inout std_logic
---         );
---end component riscv;
-
+-- Component is loaded by processor_common.vhd
 signal clk : std_logic;
 signal areset : std_logic;
 signal gpioapin : data_type;
@@ -152,20 +81,49 @@ begin
     generic map (
               -- Oscillator at 50 MHz
               SYSTEM_FREQUENCY => 50000000,
+              -- Frequency of clock() et al. KEEP THIS TO 1M
               CLOCK_FREQUENCY => 1000000,
+              -- Do we have RISC-V embedded 916 registers)?
               HAVE_RISCV_E => false,
+              -- Do we have integer hardware multiply/divide?
               HAVE_MULDIV => TRUE,
+              -- Do we have the fast divider?
               FAST_DIVIDE => TRUE,
-              HAVE_ZBA => TRUE,
+              -- Do we have the Zba extension?
+              HAVE_ZBA => false,
+              -- Do we have vectored MTVEC (for interrupts)?
               VECTORED_MTVEC => TRUE,
+              -- Do we have registers in onboard RAM?
               HAVE_REGISTERS_IN_RAM => TRUE,
+              -- Do we have the buildin bootloader?
               HAVE_BOOTLOADER_ROM => false,
+              -- Number of address bits for ROM
+              ROM_ADDRESS_BITS => 16,
+              -- Number of address bits for RAM
+              RAM_ADDRESS_BITS => 15,
+              -- 4 high bits of ROM address
+              ROM_HIGH_NIBBLE => x"0",
+              -- 4 high bits of boot ROM address
+              BOOT_HIGH_NIBBLE => x"1",
+              -- 4 high bits of RAM address
+              RAM_HIGH_NIBBLE => x"2",
+              -- 4 high bits of I/O address
+              IO_HIGH_NIBBLE => x"F",
+              -- Do we have fast store?
+              HAVE_FAST_STORE => false,
+              -- Use UART1?
               HAVE_UART1 => TRUE,
+              -- Use SPI1?
               HAVE_SPI1 => TRUE,
+              -- Use SPI2?
               HAVE_SPI2 => TRUE,
+              -- Use I2C1?
               HAVE_I2C1 => TRUE,
+              -- Use I2C2?
               HAVE_I2C2 => TRUE,
+              -- Use Timer 1?
               HAVE_TIMER1 => TRUE,
+              -- Use Timer 2?
               HAVE_TIMER2 => TRUE
              )
     port map (I_clk => clk,

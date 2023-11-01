0c2e39955904f0ae8737e7a874b9ab5adf919168
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2023-11-01 13:34:50 +0100

    Change URL of github repository

--- rtl/thuas-riscv/address_decode.vhd
+++ rtl/thuas-riscv/address_decode.vhd
@@ -31,7 +31,7 @@
 -- # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED  #
 -- # OF THE POSSIBILITY OF SUCH DAMAGE.                                                            #
 -- # ********************************************************************************************* #
--- # https:/github.com/jesseopdenbrouw/riscv-rv32                                                  #
+-- # https:/github.com/jesseopdenbrouw/thuas-riscv                                                 #
 -- #################################################################################################
 
 -- This file contains the description of address decoder and

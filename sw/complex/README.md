e27874d9fac694d4a357c1f369c02de42d884aee
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2023-11-12 17:24:01 +0100

    New program: testing complex doublles

---
+++ sw/complex/README.md
@@ -0,0 +1,13 @@
+
+# complex
+
+Calculation with complex numbers
+
+# Description
+
+This program uses the buildin complex functionality of C.
+It calculates the result of e^(i.pi), which is -1 (Euler's identity).
+
+# Status
+
+Works on the board.
---
+++ sw/complex/complex.c
@@ -0,0 +1,18 @@
+
+/* check that exp(i * pi) == -1 */
+#include <math.h>        /* for atan */
+#include <stdio.h>
+#include <complex.h>
+
+#include <thuasrv32.h>
+
+int main(void)
+{
+	double pi = 4 * atan(1.0);
+	double complex z = cexp(I * pi);
+
+	uart1_init(BAUD_RATE, UART_CTRL_NONE);
+
+	uart1_printf("%f + %f * i\r\n", creal(z), cimag(z));
+}
+

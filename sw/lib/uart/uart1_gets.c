c338dbb5bc373cf7f22e3758c9aa8644bd78cce2
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2024-03-20 17:01:58 +0100

    Indenting

--- sw/lib/uart/uart1_gets.c
+++ sw/lib/uart/uart1_gets.c
@@ -29,38 +29,38 @@ int uart1_gets(char buffer[], int size)
 		switch (chr) {
 			case '\n':
 			case '\r':	buffer[index] = '\0';
-					uart1_puts("\r\n");
-					return index;
-					break;
+						uart1_puts("\r\n");
+						return index;
+						break;
 			/* Backspace key */
 			case 0x7f:
 			case '\b':	if (index>0) {
-						uart1_putc(0x7f);
-						index--;
-					} else {
-						uart1_putc('\a');
-					}
-					break;
+							uart1_putc(0x7f);
+							index--;
+						} else {
+							uart1_putc('\a');
+						}
+						break;
 			/* control-U */
 			case 21:	while (index>0) {
-						uart1_putc(0x7f);
-						index--;
-					}
-					break;
-			/* control-C */
-			case 0x03:  	uart1_puts("<break>\r\n");
-					index=0;
-					break;
-			default:	if (index<size-1) {
-						if (chr>0x1f && chr<0x7f) {
-							buffer[index] = chr;
-							index++;
-							uart1_putc(chr);
+							uart1_putc(0x7f);
+							index--;
 						}
-					} else {
-						uart1_putc('\a');
-					}
-					break;
+						break;
+			/* control-C */
+			case 0x03: 	uart1_puts("<break>\r\n");
+						index=0;
+						break;
+			default:	if (index<size-1) {
+							if (chr>0x1f && chr<0x7f) {
+								buffer[index] = chr;
+								index++;
+								uart1_putc(chr);
+							}
+						} else {
+							uart1_putc('\a');
+						}
+						break;
 		}
 	}
 	return index;

62a211d7f80ae959d4c4ed666ab2973885bf1b29
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2024-04-27 22:34:52 +0200

    Added new counting event

--- sw/mhpmcounter/mhpmcounter.c
+++ sw/mhpmcounter/mhpmcounter.c
@@ -20,19 +20,21 @@ int main(int argc, char *argv[])
 
 	set_mtvec(trap_handler, TRAP_DIRECT_MODE);
 
-	csr_write(mhpmevent3, 1);
-	csr_write(mhpmevent4, 2);
-	csr_write(mhpmevent5, 4);
-	csr_write(mhpmevent6, 8);
-	csr_write(mhpmevent7, 16);
-	csr_write(mhpmevent8, 32);
-	csr_write(mhpmevent9, 64);
+	csr_write(mhpmevent3, CSR_HPM_JUMP);
+	csr_write(mhpmevent4, CSR_HPM_STALLS);
+	csr_write(mhpmevent5, CRR_HPM_STORES);
+	csr_write(mhpmevent6, CSR_HPM_LOADS);
+	csr_write(mhpmevent7, CSR_HPM_ECALLS);
+	csr_write(mhpmevent8, CSR_HPM_EBREAKS);
+	csr_write(mhpmevent9, CSR_HPM_MULDIV);
 
 	uart1_puts("c3 counts branch/jump\r\n");
 	uart1_puts("c4 counts stall cycles\r\n");
 	uart1_puts("c5 counts stores\r\n");
 	uart1_puts("c6 counts loads\r\n");
 	uart1_puts("c7 counts ECALLs\r\n");
+	uart1_puts("c8 counts EBREAKs\r\n");
+	uart1_puts("c9 counts multiply/divides\r\n");
 
 	while (1) {
 
@@ -46,6 +48,10 @@ int main(int argc, char *argv[])
 		uart1_printulonglong(csr_get_mhpmcounter6());
 		uart1_puts(", c7: ");
 		uart1_printulonglong(csr_get_mhpmcounter7());
+		uart1_puts(", c8: ");
+		uart1_printulonglong(csr_get_mhpmcounter8());
+		uart1_puts(", c9: ");
+		uart1_printulonglong(csr_get_mhpmcounter9());
 		uart1_puts("     \r");
 		count++;
 		if (count == 400) {

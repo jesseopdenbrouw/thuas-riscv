8a3746f63720ac36a7f3371229fff46d59eea166
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2024-12-14 14:54:55 +0100

    New addresses for system timer,  new indent defines

--- sw/FreeRTOSdemo/FreeRTOSConfig.h
+++ sw/FreeRTOSdemo/FreeRTOSConfig.h
@@ -98,64 +98,64 @@
  *   but the frequency of the MTIME counter (prescaled from the
  *   processor clock).
  */
-#define configMTIME_BASE_ADDRESS 	( 0xf00000f0UL  )
-#define configMTIMECMP_BASE_ADDRESS ( 0xf00000f8UL  )
+#define configMTIME_BASE_ADDRESS    ( 0xf0000a00UL  )
+#define configMTIMECMP_BASE_ADDRESS ( 0xf0000a08UL  )
 
-#define configISR_STACK_SIZE_WORDS ( 128 )
+#define configISR_STACK_SIZE_WORDS  ( 128 )
 
-#define configUSE_PREEMPTION			1
-#define configUSE_IDLE_HOOK				1
-#define configUSE_TICK_HOOK				1
-#define configCPU_CLOCK_HZ				1000000 /* INTERNAL TIME/TIMEH count frequency */
-#define configTICK_RATE_HZ				( ( TickType_t ) 100 )
-#define configMAX_PRIORITIES			( 5 )
-#define configMINIMAL_STACK_SIZE		( ( unsigned short ) 128 ) /* Can be as low as 60 but some of the demo tasks that use this constant require it to be higher. */
+#define configUSE_PREEMPTION              1
+#define configUSE_IDLE_HOOK               1
+#define configUSE_TICK_HOOK               1
+#define configCPU_CLOCK_HZ                1000000 /* INTERNAL TIME/TIMEH count frequency */
+#define configTICK_RATE_HZ                ( ( TickType_t ) 100 )
+#define configMAX_PRIORITIES              ( 5 )
+#define configMINIMAL_STACK_SIZE          ( ( unsigned short ) 128 ) /* Can be as low as 60 but some of the demo tasks that use this constant require it to be higher. */
 #define configSUPPORT_DYNAMIC_ALLOCATION  1
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 25 * 1024 ) ) /* Must be less that 32 kB, so 25 kB is a good size */
-#define configMAX_TASK_NAME_LEN			( 16 )
-#define configUSE_TRACE_FACILITY		1
-#define configUSE_16_BIT_TICKS			0
-#define configIDLE_SHOULD_YIELD			0
-#define configUSE_MUTEXES				1
-#define configQUEUE_REGISTRY_SIZE		8
-#define configCHECK_FOR_STACK_OVERFLOW	2
-#define configUSE_RECURSIVE_MUTEXES		1
-#define configUSE_MALLOC_FAILED_HOOK	1
-#define configUSE_APPLICATION_TASK_TAG	0
-#define configUSE_COUNTING_SEMAPHORES	1
-#define configGENERATE_RUN_TIME_STATS	0
-#define configTASK_NOTIFICATION_ARRAY_ENTRIES 4
+#define configTOTAL_HEAP_SIZE             ( ( size_t ) ( 25 * 1024 ) ) /* Must be less that 32 kB, so 25 kB is a good size */
+#define configMAX_TASK_NAME_LEN           ( 16 )
+#define configUSE_TRACE_FACILITY          1
+#define configUSE_16_BIT_TICKS            0
+#define configIDLE_SHOULD_YIELD           0
+#define configUSE_MUTEXES                 1
+#define configQUEUE_REGISTRY_SIZE         8
+#define configCHECK_FOR_STACK_OVERFLOW    2
+#define configUSE_RECURSIVE_MUTEXES       1
+#define configUSE_MALLOC_FAILED_HOOK      1
+#define configUSE_APPLICATION_TASK_TAG    0
+#define configUSE_COUNTING_SEMAPHORES     1
+#define configGENERATE_RUN_TIME_STATS     0
+#define configTASK_NOTIFICATION_ARRAY_ENTRIES   4
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 1
 
 /* Co-routine definitions. */
-#define configUSE_CO_ROUTINES 			0
+#define configUSE_CO_ROUTINES             0
 #define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
 
 /* Software timer definitions. */
-#define configUSE_TIMERS				1
-#define configTIMER_TASK_PRIORITY		( configMAX_PRIORITIES - 1 )
-#define configTIMER_QUEUE_LENGTH		4
-#define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE )
+#define configUSE_TIMERS                1
+#define configTIMER_TASK_PRIORITY       ( configMAX_PRIORITIES - 1 )
+#define configTIMER_QUEUE_LENGTH        4
+#define configTIMER_TASK_STACK_DEPTH    ( configMINIMAL_STACK_SIZE )
 
 /* Task priorities.  Allow these to be overridden. */
 #ifndef uartPRIMARY_PRIORITY
-	#define uartPRIMARY_PRIORITY		( configMAX_PRIORITIES - 3 )
+    #define uartPRIMARY_PRIORITY        ( configMAX_PRIORITIES - 3 )
 #endif
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet			1
-#define INCLUDE_uxTaskPriorityGet			1
-#define INCLUDE_vTaskDelete					1
-#define INCLUDE_vTaskCleanUpResources		1
-#define INCLUDE_vTaskSuspend				1
-#define INCLUDE_vTaskDelayUntil				1
-#define INCLUDE_vTaskDelay					1
-#define INCLUDE_eTaskGetState				1
-#define INCLUDE_xTimerPendFunctionCall		1
-#define INCLUDE_xTaskAbortDelay				1
-#define INCLUDE_xTaskGetHandle				1
-#define INCLUDE_xSemaphoreGetMutexHolder	1
+#define INCLUDE_vTaskPrioritySet         1
+#define INCLUDE_uxTaskPriorityGet        1
+#define INCLUDE_vTaskDelete              1
+#define INCLUDE_vTaskCleanUpResources    1
+#define INCLUDE_vTaskSuspend             1
+#define INCLUDE_vTaskDelayUntil          1
+#define INCLUDE_vTaskDelay               1
+#define INCLUDE_eTaskGetState            1
+#define INCLUDE_xTimerPendFunctionCall   1
+#define INCLUDE_xTaskAbortDelay          1
+#define INCLUDE_xTaskGetHandle           1
+#define INCLUDE_xSemaphoreGetMutexHolder 1
 
 /* Normal assert() semantics without relying on the provision of an assert.h
 header file. */

01055c14b6586e422e949bbbeec653698928cf7f
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2025-01-01 17:55:28 +0100

    Bump version

--- rtl/thuas-riscv/processor_common.vhd
+++ rtl/thuas-riscv/processor_common.vhd
@@ -44,7 +44,7 @@ use ieee.numeric_std.all;
 package processor_common is
 
     -- Hardware version, BCD encoded
-    constant HW_VERSION : integer := 16#01_01_00_02#;
+    constant HW_VERSION : integer := 16#01_01_00_03#;
 
     
     -- Used data types

0abb8d70a0b44b95079e02a8cbf36051d28c033a
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2026-06-14 11:29:30 +0200

    Image for the RAM

---
+++ rtl/thuas-riscv/ram_image.vhd
@@ -0,0 +1,54 @@
+-- #################################################################################################
+-- # ram_image.vhd - Image for the RAM                                                             #
+-- # ********************************************************************************************* #
+-- # This file is part of the THUAS RISCV RV32 Project                                             #
+-- # ********************************************************************************************* #
+-- # BSD 3-Clause License                                                                          #
+-- #                                                                                               #
+-- # Copyright (c) 2026, Jesse op den Brouw. All rights reserved.                                  #
+-- #                                                                                               #
+-- # Redistribution and use in source and binary forms, with or without modification, are          #
+-- # permitted provided that the following conditions are met:                                     #
+-- #                                                                                               #
+-- # 1. Redistributions of source code must retain the above copyright notice, this list of        #
+-- #    conditions and the following disclaimer.                                                   #
+-- #                                                                                               #
+-- # 2. Redistributions in binary form must reproduce the above copyright notice, this list of     #
+-- #    conditions and the following disclaimer in the documentation and/or other materials        #
+-- #    provided with the distribution.                                                            #
+-- #                                                                                               #
+-- # 3. Neither the name of the copyright holder nor the names of its contributors may be used to  #
+-- #    endorse or promote products derived from this software without specific prior written      #
+-- #    permission.                                                                                #
+-- #                                                                                               #
+-- # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS   #
+-- # OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF               #
+-- # MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE    #
+-- # COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,     #
+-- # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE #
+-- # GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED    #
+-- # AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     #
+-- # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED  #
+-- # OF THE POSSIBILITY OF SUCH DAMAGE.                                                            #
+-- # ********************************************************************************************* #
+-- # https:/github.com/jesseopdenbrouw/thuas-riscv                                                 #
+-- #################################################################################################
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+library work;
+use work.processor_common.all;
+
+package ram_image is
+
+    -- Use a little trick to create an empty RAM image
+    -- The range is deliberately 1 to 0 so that the effective
+    -- range is empty (NULL). A constant must have an
+    -- initializer, but there are no elements. The 'others'
+    -- is used to adhere to the VHDL syntax, but effectively
+    -- is ignored.
+    -- You will see a warning reported by the synthesizer.
+    constant ram_contents : memory_type(1 to 0) := (others => x"00000000");
+
+end package ram_image;

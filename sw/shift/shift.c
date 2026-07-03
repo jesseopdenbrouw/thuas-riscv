da3fc8c17eab04677f6de82bf73e78c4509468b2
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2026-07-03 18:50:13 +0200

    Add more shifts (for in simulator)

--- sw/shift/shift.c
+++ sw/shift/shift.c
@@ -6,8 +6,10 @@ int main(void) {
 	volatile int32_t y = -31;
 	volatile uint64_t z = 0xff00ff00ff00ff00;
 
-	x = x << 4;
-	y = y >> 4;
+	for (int i = 0; i < 32; i++) {
+		x = 1 << i;
+		y = y >> 1;
+	}
 
 	z = z >> 31;
 }
